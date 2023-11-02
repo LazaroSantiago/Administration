d38c3d7d73cd242fe1ac6711cb5bbd39d37969a8
Santiago Lázaro <santi.lazaro@gmail.com>
2023-11-01 23:59:55 -0300

    setTripPrice() terminado

--- src/main/java/controller/AdministratorController.java
+++ src/main/java/controller/AdministratorController.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import service.AdministratorService;
 
+import java.time.LocalDateTime;
+
 @Controller("AdministratorController")
 @RequestMapping("/admin")
 public class AdministratorController {
@@ -53,20 +55,73 @@ public class AdministratorController {
 //    b. Como administrador quiero poder anular cuentas
 //    para inhabilitar el uso momentáneo de la misma
     @PutMapping("/ban/{id}")
-    public ResponseEntity<?> banAccount(@PathVariable Long id){
-        try{
+    public ResponseEntity<?> banAccount(@PathVariable Long id) {
+        try {
             return ResponseEntity.status(HttpStatus.OK).body(administratorService.banAccount(id));
-        } catch (Exception e){
+        } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"error\":\"Error. Por favor intente más tarde.\"}");
         }
     }
 
     @PutMapping("/unban/{id}")
-    public ResponseEntity<?> unbanAccount(@PathVariable Long id){
-        try{
+    public ResponseEntity<?> unbanAccount(@PathVariable Long id) {
+        try {
             return ResponseEntity.status(HttpStatus.OK).body(administratorService.unbanAccount(id));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"error\":\"Error. Por favor intente más tarde.\"}");
+        }
+    }
+
+//  e.Como administrador quiero consultar la cantidad de monopatines actualmente en operación,
+//    versus la cantidad de monopatines actualmente en mantenimiento.
+    @GetMapping("/functional_scooters")
+    public ResponseEntity<?> getFunctionalScooters() {
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(administratorService.getFunctionalScooters());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"error\":\"Error. Por favor intente más tarde.\"}");
+        }
+    }
+
+    //  Como administrador quiero consultar los monopatines con más de X viajes en un cierto año.
+    @GetMapping("/most_used_scooters/trips/{trips}/year/{year}")
+    public ResponseEntity<?> getScootersByTripAmount(@PathVariable Long trips,@PathVariable  int year) {
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(administratorService.getScootersByTripAmount(trips, year));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"error\":\"Error. Por favor intente más tarde.\"}");
+        }
+    }
+
+//  Como administrador quiero consultar el total facturado en un rango de meses de cierto año.
+/*
+    @GetMapping("/profit/months/{months}/year/{year}")
+    public ResponseEntity<?> getProfits(int month1, int month2, int year){
+        try{
+            return ResponseEntity.status(HttpStatus.OK).body(administratorService.getProfits(month1, month2, year));
         } catch (Exception e){
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"error\":\"Error. Por favor intente más tarde.\"}");
         }
     }
+*/
+
+//  Como administrador quiero hacer un ajuste de precios, y que a partir de cierta fecha el sistema
+//  habilite los nuevos precios
+    @PutMapping("/normal_price/{price}")
+    public ResponseEntity<?> setNormalTripPrice(LocalDateTime date,@PathVariable  Long price) {
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(administratorService.setNormalTripPrice(date, price));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"error\":\"Error. Por favor intente más tarde.\"}");
+        }
+    }
+    @PutMapping("/extra_price/{price}")
+    public ResponseEntity<?> setExtraTripPrice(LocalDateTime date,@PathVariable Long price) {
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(administratorService.setNormalTripPrice(date, price));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"error\":\"Error. Por favor intente más tarde.\"}");
+        }
+    }
+
 }
--- src/main/java/service/AdministratorService.java
+++ src/main/java/service/AdministratorService.java
@@ -1,5 +1,6 @@
 package service;
 
+import dto.ScooterDTO;
 import entity.Administrator;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import repository.AdministratorRepository;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,7 +21,7 @@ public class AdministratorService implements BaseService<Administrator> {
     @Autowired
     private AdministratorRepository administratorRepository;
 
-    private RestTemplate restTemplate;
+    private final RestTemplate restTemplate;
 
     @Autowired
     public AdministratorService(RestTemplateBuilder builder) {
@@ -69,23 +73,75 @@ public class AdministratorService implements BaseService<Administrator> {
     @Transactional
     public ResponseEntity<?> banAccount(Long id) {
         String url = "localhost:8083/accounts/ban/{" + id + "}";
-        HttpHeaders headers = new HttpHeaders();
-        HttpEntity<Void> requestEntity = new HttpEntity<>(headers);
-        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<Void> requestEntity = getRequestEntity();
         return restTemplate.exchange(url, HttpMethod.PUT, requestEntity, String.class);
-
-        //url, HttpMethod.PUT, headers, String.class
-
-        //llamar al endpoint de Account
-        //desde ahi, setear la cuenta como baneada
     }
 
     @Transactional
     public ResponseEntity<?> unbanAccount(Long id) {
         String url = "localhost:8083/accounts/unban/{" + id + "}";
+        HttpEntity<Void> requestEntity = getRequestEntity();
+        return restTemplate.exchange(url, HttpMethod.PUT, requestEntity, String.class);
+    }
+
+    private static HttpEntity<Void> getRequestEntity() {
         HttpHeaders headers = new HttpHeaders();
         HttpEntity<Void> requestEntity = new HttpEntity<>(headers);
         headers.setContentType(MediaType.APPLICATION_JSON);
+        return requestEntity;
+    }
+
+    @Transactional
+    public ResponseEntity<?> getFunctionalScooters(){
+        String url = "localhost:8082/scooters/functional_scooters";
+        return restTemplate.getForEntity(url, Integer[].class);
+    }
+
+    @Transactional
+    public List<ScooterDTO> getScootersByTripAmount(Long trips, int year) throws Exception {
+        //TODO: url
+        String url = "localhost:8082/scooters/most_used_scooters/trips/{"+trips+"}/year/{"+year+"}}";
+        ResponseEntity<ScooterDTO[]> responseEntity = restTemplate.getForEntity(url, ScooterDTO[].class);
+
+        if (responseEntity.getStatusCode().is2xxSuccessful()) {
+            ScooterDTO[] scooterArray = responseEntity.getBody();
+            assert scooterArray != null;
+            return Arrays.asList(scooterArray);
+        } else {
+            throw new Exception("Error al obtener datos del microservicio.");
+        }
+    }
+
+    /*
+    public Long getProfits(int month1, int month2, int year){
+        //TODO: url
+        //creo que se va a llamar a trip o recepit en vez de scooters
+        String url = "localhost:8082/scooters/most_used_scooters/trips/{"+trips+"}/year/{"+year+"}}";
+        ResponseEntity<Long> responseEntity = restTemplate.getForEntity(url, Long.class);
+        return responseEntity.getBody();
+    }*/
+
+    //Como administrador quiero hacer un ajuste de precios, y que a partir de cierta fecha el sistema
+    //habilite los nuevos precios
+    @Transactional
+    public ResponseEntity<?> setNormalTripPrice(LocalDateTime date, Long price) throws InterruptedException {
+        long time = getTime(date);
+        Thread.sleep(time);
+        String url = "localhost:8082/scooters/normalRate/{"+price+"}";
+        HttpEntity<Void> requestEntity = getRequestEntity();
         return restTemplate.exchange(url, HttpMethod.PUT, requestEntity, String.class);
+    }
+
+    @Transactional
+    public ResponseEntity<?> setExtraTripPrice(LocalDateTime date, Long price) throws InterruptedException {
+        long time = getTime(date);
+        Thread.sleep(time);
+        String url = "localhost:8082/scooters/extraRate/{"+price+"}";
+        HttpEntity<Void> requestEntity = getRequestEntity();
+        return restTemplate.exchange(url, HttpMethod.PUT, requestEntity, String.class);
+    }
+
+    private static long getTime(LocalDateTime date) {
+        return ChronoUnit.MILLIS.between(LocalDateTime.now(), date);
     }
 }
