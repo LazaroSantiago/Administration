e1201f330b34f208ef6383f9a8b9aba17efda3bb
kevinVigil <kevinvigil27@gmail.com>
2023-11-15 12:58:53 -0300

    swagger funcionando faltan detalles

--- src/main/java/com/example/administration/AdministrationApplication.java
+++ src/main/java/com/example/administration/AdministrationApplication.java
@@ -1,30 +1,23 @@
 package com.example.administration;
 
-import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Autowired;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import util.Test;
 
 @SpringBootApplication
-@ComponentScan({"util", "controller", "entity", "repository", "service", "com.example.administration"})
+@ComponentScan({ "controller", "entity", "repository", "service", "com.example.administration"})
 @EnableJpaRepositories("repository")
 @EnableMongoRepositories("repository")
+@OpenAPIDefinition
 @EntityScan("entity")
 public class AdministrationApplication {
-    @Autowired
-    private Test test;
 
     public static void main(String[] args) {
         SpringApplication.run(AdministrationApplication.class, args);
     }
 
-    @PostConstruct
-    public void init() {
-//        test.test();
-    }
 }
--- src/main/java/controller/AdministratorController.java
+++ src/main/java/controller/AdministratorController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import entity.Administrator;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +19,7 @@ public class AdministratorController {
     private final AdministratorService administratorService;
 
     @GetMapping("")
+    @Operation(summary = "Obtener todos los administradores")
     public ResponseEntity<?> findAll() {
         try {
             return ResponseEntity.status(HttpStatus.OK).body(administratorService.findAll());
@@ -27,6 +29,7 @@ public class AdministratorController {
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Obtener un administradores por ID")
     public ResponseEntity<?> findById(@PathVariable Long id) {
         try {
             return ResponseEntity.status(HttpStatus.OK).body(administratorService.findById(id));
@@ -36,6 +39,7 @@ public class AdministratorController {
     }
 
     @PostMapping("")
+    @Operation(summary = "Crear un nuevo administrador")
     public ResponseEntity<?> save(@RequestBody Administrator entity) {
         try {
             return ResponseEntity.status(HttpStatus.OK).body(administratorService.save(entity));
@@ -45,6 +49,7 @@ public class AdministratorController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Borrar un administrador por id")
     public ResponseEntity<?> deleteById(@PathVariable Long id) {
         try {
             return ResponseEntity.status(HttpStatus.OK).body(administratorService.delete(id));
@@ -56,6 +61,7 @@ public class AdministratorController {
 //    b. Como administrador quiero poder anular cuentas
 //    para inhabilitar el uso momentáneo de la misma
     @PutMapping("/ban/{id}")
+    @Operation(summary = "Anular cuenta")
     public ResponseEntity<?> banAccount(@PathVariable Long id) {
         try {
             return ResponseEntity.status(HttpStatus.OK).body(administratorService.banAccount(id));
@@ -65,6 +71,7 @@ public class AdministratorController {
     }
 
     @PutMapping("/unban/{id}")
+    @Operation(summary = "Habilitar cuentas")
     public ResponseEntity<?> unbanAccount(@PathVariable Long id) {
         try {
             return ResponseEntity.status(HttpStatus.OK).body(administratorService.unbanAccount(id));
@@ -76,6 +83,7 @@ public class AdministratorController {
 //  e.Como administrador quiero consultar la cantidad de monopatines actualmente en operación,
 //    versus la cantidad de monopatines actualmente en mantenimiento.
     @GetMapping("/functional_scooters")
+    @Operation(summary = "Consultar la cantidad de monopatines en operación")
     public ResponseEntity<?> getFunctionalScooters() {
         try {
             return ResponseEntity.status(HttpStatus.OK).body(administratorService.getFunctionalScooters());
@@ -86,6 +94,7 @@ public class AdministratorController {
 
     //  Como administrador quiero consultar los monopatines con más de X viajes en un cierto año.
     @GetMapping("/most_used_scooters/trips/{trips}/year/{year}")
+    @Operation(summary = "Consultar los monopatines con más de X viajes en un cierto año")
     public ResponseEntity<?> getScootersByTripAmount(@PathVariable Long trips,@PathVariable  int year) {
         try {
             return ResponseEntity.status(HttpStatus.OK).body(administratorService.getScootersByTripAmount(trips, year));
@@ -109,6 +118,7 @@ public class AdministratorController {
 //  Como administrador quiero hacer un ajuste de precios, y que a partir de cierta fecha el sistema
 //  habilite los nuevos precios
     @PutMapping("/normal_price/{price}")
+    @Operation(summary = "Hacer un ajuste del precio normal a partir de cierta fecha dada")
     public ResponseEntity<?> setNormalTripPrice(LocalDateTime date,@PathVariable  Long price) {
         try {
             return ResponseEntity.status(HttpStatus.OK).body(administratorService.setNormalTripPrice(date, price));
@@ -116,7 +126,9 @@ public class AdministratorController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"error\":\"Error. Por favor intente más tarde.\"}");
         }
     }
+
     @PutMapping("/extra_price/{price}")
+    @Operation(summary = "Hacer un ajuste del precio extra a partir de cierta fecha dada")
     public ResponseEntity<?> setExtraTripPrice(LocalDateTime date,@PathVariable Long price) {
         try {
             return ResponseEntity.status(HttpStatus.OK).body(administratorService.setNormalTripPrice(date, price));
